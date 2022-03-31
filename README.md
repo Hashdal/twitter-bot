584f90012826ea0cd2dfedca01ee62faf74068da
Ali Asadi <asadyali80@gmail.com>
2022-03-31 20:10:00 +0430

    add mode in config.json

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # twitter-bot
+## hwo to use:
+there is a "mode" field in config.json file, for sending tweet enter 1, for send random tweet from enter 2 and for send random tweets in timespans enter 3.
--- botActions.py
+++ botActions.py
@@ -9,6 +9,7 @@ class MainActions:
     __db = None
     __delays = None
     __numberOfTweets = None
+    mode = None
     def __init__(self, configFile: str) -> None:
         botInfo = json.loads(open(configFile, 'r').read())
         self.__api = twitter.Api(
@@ -20,6 +21,7 @@ class MainActions:
         self.__db = accessDB.Database(configFile=configFile)
         self.__delays = int(botInfo["delay_seconds"])
         self.__numberOfTweets = int(botInfo["number_of_tweets"])
+        self.mode = int(botInfo["mode"])
     
     def random_from_db(self) -> None:
         self.__api.PostUpdate(self.__db.read_random_data_from_table())
@@ -40,11 +42,10 @@ class MainActions:
 
 
 
-a = input('1. send tweet, 2. send random tweet from database, 3. send random tweets in timespans: ')
 m = MainActions('./config.json')
-if a == '1':
+if m.mode == 1:
     m.send_tweet()
-elif a == '2':
+elif m.mode == 2:
     m.random_from_db()
-else:
+elif m.mode == 3:
     m.send_tweet_in_timespans()
